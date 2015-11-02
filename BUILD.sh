b4254b48854af76fb052f1e6d3e83f8890070db7
Constantine Linnick <theaspect@gmail.com>
2015-11-02 17:04:41 +0300

    Fix build script

--- BUILD.sh
+++ BUILD.sh
@@ -3,7 +3,7 @@ rm -rf "out"
 mkdir -p "out/classes"
 find . -name "*.java" | xargs javac -cp "lib/*" -d out/classes -sourcepath src -verbose
 
-cp -r resources/ out/classes/
+cp -r resources/* out/classes/
 
 mkdir -p "out/lib"
 cp lib/* out/lib/
